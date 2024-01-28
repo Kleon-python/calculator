c091ef6e4d61b472565eb551f7bb079f99e7c8c7
Kleon-python <157064392+Kleon-python@users.noreply.github.com>
2024-01-28 21:41:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-
 function appendToDisplay(value) {
-    document.getElementById('display').value += value;
+    document.getElementById('display').innerText += value;
 }
 
 function clearDisplay() {
-    document.getElementById('display').value = '';
+    document.getElementById('display').innerText = '';
 }
 
 function calculateResult() {
     try {
         // Use eval for basic mathematical evaluations
-        document.getElementById('display').value = eval(document.getElementById('display').value);
+        document.getElementById('display').innerText = eval(document.getElementById('display').innerText);
     } catch (error) {
-        document.getElementById('display').value = 'Error';
+        document.getElementById('display').innerText = 'Error';
     }
 }
-
+function backspace() {
+    var currentDisplay = document.getElementById('display').innerText;
+    document.getElementById('display').innerText = currentDisplay.slice(0, -1);
+}
